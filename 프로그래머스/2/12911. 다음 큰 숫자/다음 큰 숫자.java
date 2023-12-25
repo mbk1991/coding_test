59ed0cd9149c934229c2678670a580316e0cacaa
mbk1991 <mbk1991@naver.com>
2023-12-25 12:03:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,47 @@
+//231225 11:23, 1차 시도 시간 초과
+class Solution {
+    public int solution(int n) {
+        int answer = searchBiggerN(n);
+        return answer;
+    }
+    
+    private int searchBiggerN(int n){
+        int countOneOfN = countOneOfBinary(n);
+        for(int i=n+1;;i++){
+            if(countOneOfBinary(i) == countOneOfN){
+                return i;
+            }
+        }
+    }
+    
+    private int countOneOfBinary(int n){
+        int cnt=0;
+        for(int i=n;i>1;){
+            cnt += i%2;
+            i = i/2;
+        }
+        return cnt+1;
+    }
+}
+
+
+
+// 1차 시도 : 시간 초과
+//     public int solution(int n) {
+//         int answer = searchBiggerN(n);
+//         return answer;
+//     }
+    
+//     private int searchBiggerN(int n){
+//         int countOneOfN = countOneOfBinary(n);
+//         for(int i=n+1;;i++){
+//             if(countOneOfBinary(i) == countOneOfN){
+//                 return i;
+//             }
+//         }
+//     }
+    
+//     private int countOneOfBinary(int n){
+//         String binaryN = Integer.toBinaryString(n);
+//         return binaryN.length() - binaryN.replaceAll("1","").length();
+//     }

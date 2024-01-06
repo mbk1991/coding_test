2dd9f45207dbf56ecafa5ab14fe751d6a698ac59
mbk1991 <mbk1991@naver.com>
2024-01-06 18:57:12 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,26 @@
+/*
+*/
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++){
+            if(map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            }else{
+                map.put(clothes[i][1], 1);
+            }
+        }
+        
+        for(String s: map.keySet()){
+            answer *= map.get(s) + 1;
+        }
+        
+        return answer-1;
+    }
+}

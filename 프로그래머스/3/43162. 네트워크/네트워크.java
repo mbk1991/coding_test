a8501efdf8d5c5df46c68e7f989cb52e9ad930d7
mbk1991 <mbk1991@naver.com>
2024-02-17 23:36:26 +0900

    [level 3] Title: 네트워크, Time: 1.35 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,52 +1,94 @@
 /*2402217 10:05*/
 
-
 import java.util.*;
 
+//stack
 class Solution {
+    private static boolean[] isVisit;
+    private static Stack<Integer> st;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] isVisited = new boolean[n];
+        isVisit = new boolean[n];
+        st = new Stack<>();
         
-        Set<Integer>[] list = new HashSet[n];
-        for(int i=0; i<n; i++){
-            list[i] = new HashSet<Integer>();
-        }
         
-        for(int i=0; i<computers.length; i++){
-            for(int j=0; j<computers[0].length; j++){
-                if(i==j) continue;
-                if(computers[i][j] == 1){
-                    list[i].add(j);
-                } 
+        for(int computer=0; computer<n; computer++){
+            if(!isVisit[computer]){
+                 isVisit[computer] = true;
+                 st.push(computer);
+                
+                 stackDFS(computers);
+                 answer++;
             }
         }
-        
-        for(int i=0; i<n; i++){
-            if(isVisited[i]) continue;
-            chkNetwork(i,list,isVisited);    
-            answer++;
-        }
-        
-        System.out.println( Arrays.toString(isVisited) );
-        
-        
-        
         return answer;
     }
     
-    void chkNetwork(int computerNo, Set<Integer>[] list, boolean[] visited){
+    void stackDFS(int[][] computers){
+        while(!st.isEmpty()){
+            int computer = st.pop();
+            System.out.println(computer);
 
-        if(list[computerNo].size() < 1){
-          return;  
-        }
-        
-        if(!visited[computerNo]){
-            visited[computerNo] = true;
-            for(int computer : list[computerNo]){
-                chkNetwork(computer, list, visited);
+            for(int i=0; i<computers[computer].length; i++){
+                if((computer!=i) && (computers[computer][i] == 1)){
+                    if(!isVisit[i]){
+                        isVisit[i] = true;
+                        st.push(i);
+                    }
+                }
             }
         }
-        return;
     }
 }
+
+
+
+//recursive
+// class Solution {
+//     public int solution(int n, int[][] computers) {
+//         int answer = 0;
+//         boolean[] isVisited = new boolean[n];
+        
+//         Set<Integer>[] list = new HashSet[n];
+//         for(int i=0; i<n; i++){
+//             list[i] = new HashSet<Integer>();
+//         }
+        
+//         for(int i=0; i<computers.length; i++){
+//             for(int j=0; j<computers[0].length; j++){
+//                 if(i==j) continue;
+//                 if(computers[i][j] == 1){
+//                     list[i].add(j);
+//                 } 
+//             }
+//         }
+        
+//         for(int i=0; i<n; i++){
+//             if(isVisited[i]) continue;
+//             chkNetwork(i,list,isVisited);    
+//             answer++;
+//         }
+        
+//         System.out.println( Arrays.toString(isVisited) );
+        
+        
+        
+//         return answer;
+//     }
+    
+//     void chkNetwork(int computerNo, Set<Integer>[] list, boolean[] visited){
+
+//         if(list[computerNo].size() < 1){
+//           return;  
+//         }
+        
+//         if(!visited[computerNo]){
+//             visited[computerNo] = true;
+//             for(int computer : list[computerNo]){
+//                 chkNetwork(computer, list, visited);
+//             }
+//         }
+//         return;
+//     }
+// }

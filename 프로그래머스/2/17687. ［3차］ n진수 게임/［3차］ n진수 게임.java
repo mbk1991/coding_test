2d85389cae5cb6b84b8efd641d487db2c71ec5e4
mbk1991 <mbk1991@naver.com>
2024-02-18 09:47:16 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.65 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<=100_000; i++){
+            sb.append(Integer.toString(i,n).toUpperCase());
+            if(sb.length() > t*m) break;
+        }
+        
+        char[] cArr = sb.toString().toCharArray();
+        StringBuilder tube = new StringBuilder();
+        for(int i=p-1; i<cArr.length; i+=m){
+            if(tube.length() < t) tube.append(cArr[i]);
+        }
+        answer = tube.toString();
+        
+        return answer;
+    }
+}

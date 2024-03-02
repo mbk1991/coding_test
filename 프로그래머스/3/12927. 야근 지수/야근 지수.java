5d19c05903eec59729e94e252541ab7b44edd388
mbk1991 <mbk1991@naver.com>
2024-03-02 15:07:21 +0900

    [level 3] Title: 야근 지수, Time: 134.57 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        
+        Arrays.sort(works);
+        
+        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i:works){
+            pq.offer(i);
+        }
+        
+        while(n>0){
+            if(pq.peek() == 0) break;
+            Integer a = pq.poll();
+            pq.offer(--a);
+            --n;
+        }
+        
+        System.out.println(pq);
+        
+        while(pq.size()>0){
+            int p = pq.poll();
+            answer+= p*p;
+        }
+        return answer;
+    }
+
+}
